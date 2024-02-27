1c6730421ec54707e108762c4a4125d56c6fa467
Sayli <hessabilyas@gmail.com>
2024-02-28 00:24:06 +0100

    controller test return 404 instead of 200

--- src/test/java/fr/madeit/arosaje/ControllerTests/AnnounceControllerTest.java
+++ src/test/java/fr/madeit/arosaje/ControllerTests/AnnounceControllerTest.java
@@ -1,4 +1,26 @@
 package fr.madeit.arosaje.ControllerTests;
 
+import fr.madeit.arosaje.Controller.AnnounceController;
+import fr.madeit.arosaje.SRV.AnnounceService;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(controllers = AnnounceController.class)
 public class AnnounceControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private AnnounceService announceService;
+
+    @Test
+    public void testGetAllAnnounces() throws Exception {
+        mockMvc.perform(get("/api/announces"))
+                .andExpect(status().isOk());
+    }
 }
--- src/test/java/fr/madeit/arosaje/ControllerTests/UserControllerTest.java
+++ src/test/java/fr/madeit/arosaje/ControllerTests/UserControllerTest.java
@@ -1,4 +1,30 @@
 package fr.madeit.arosaje.ControllerTests;
 
+import fr.madeit.arosaje.Config.JwtRequestFilter;
+import fr.madeit.arosaje.Controller.UserController;
+import fr.madeit.arosaje.SRV.CustomUserDetailsService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+@WebMvcTest(controllers = UserController.class)
 public class UserControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+
+    @MockBean
+    private CustomUserDetailsService userService;
+
+    @MockBean
+     private JwtRequestFilter jwtRequestFilter;
+
+    @Test
+    public void testGetAllUsers() throws Exception {
+        mockMvc.perform(get("/api/user"))
+                .andExpect(status().isOk());
+    }
 }
