a48e37c8638aef83f79c61977b2a23b102979d05
Sayli <hessabilyas@gmail.com>
2024-02-29 18:28:41 +0100

    FileUploadControllerTest dont work

--- src/test/java/fr/madeit/arosaje/ControllerTests/FileUploadControllerTest.java
+++ src/test/java/fr/madeit/arosaje/ControllerTests/FileUploadControllerTest.java
@@ -1,6 +1,5 @@
 package fr.madeit.arosaje.ControllerTests;
 
-/*
 import fr.madeit.arosaje.Controller.FileUploadController;
 import fr.madeit.arosaje.SRV.FileStorageService;
 import org.junit.jupiter.api.Test;
@@ -8,15 +7,15 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.http.HttpHeaders;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.mock.web.MockMultipartFile;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.util.Objects;
@@ -25,9 +24,13 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@ExtendWith(MockitoExtension.class)
+@ExtendWith({MockitoExtension.class, SpringExtension.class})
+@AutoConfigureMockMvc
+@SpringBootTest
 public class FileUploadControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
     @Mock
     private FileStorageService fileStorageService;
 
@@ -60,19 +63,17 @@ public class FileUploadControllerTest {
     }
 
     private void testUploadFile(String endpoint) throws Exception {
-        Integer entityId = 1;
-        Integer userId = 1;
+        int entityId = 1;
+        int userId = 1;
 
         MockMultipartFile file = new MockMultipartFile("file", "test.txt", MediaType.TEXT_PLAIN_VALUE, "Hello, World!".getBytes());
 
         when(fileStorageService.storeFile(any(), any())).thenReturn("test.txt");
 
-        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(fileUploadController).build();
-
         MvcResult result = mockMvc.perform(MockMvcRequestBuilders.multipart(endpoint)
                         .file(file)
-                        .param("entityId", entityId.toString())
-                        .param("userId", userId.toString())
+                        .param("entityId", Integer.toString(entityId))
+                        .param("userId", Integer.toString(userId))
                         .contentType(MediaType.MULTIPART_FORM_DATA))
                 .andExpect(status().isOk())
                 .andReturn();
@@ -87,4 +88,4 @@ public class FileUploadControllerTest {
         assert fileDownloadUri.equals(expectedUri);
     }
 }
-*/
+
