b9b460253ae4c01c3a89b312e05d9a9f9d25e266
Sayli <hessabilyas@gmail.com>
2024-03-01 00:30:50 +0100

    all repository test done

--- src/test/java/fr/madeit/arosaje/DALTests/LoginRepositoryTest.java
+++ src/test/java/fr/madeit/arosaje/DALTests/LoginRepositoryTest.java
@@ -4,12 +4,10 @@ import fr.madeit.arosaje.DAL.LoginRepository;
 import fr.madeit.arosaje.BO.Login;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
